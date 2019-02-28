783c98144ade0af9dc3f95f6fd1f151b83ad5d49
Amanda Murphy <helloamandamurphy@gmail.com>
2019-02-28 19:42:35 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -80,6 +80,5 @@ def can_destroy_all_items_at_once
   10.times do |i|
     Movie.create(title: "Movie_#{i}")
   end
-  Movie.all.each{|movie|
-  movie.delete(title: "Movie_#{i}")
+  Movie.delete_all
 end
