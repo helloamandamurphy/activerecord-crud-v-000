38c573e07ac91c91e05377dd28870f178e371d66
Amanda Murphy <helloamandamurphy@gmail.com>
2019-02-28 19:38:47 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -46,7 +46,7 @@ def can_find_by_multiple_attributes
 end
 
 def can_find_using_where_clause_and_be_sorted
-  Movie.where("release_date > 2002").order(:release_date :desc)
+  Movie.where("release_date > 2002").order(release_date: :desc)
 end
 
 def can_be_found_updated_and_saved
